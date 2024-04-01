e40970108c247d4255fada3a0f785d4ee3565b11
nkkpao <99089421+nkkpao@users.noreply.github.com>
2024-04-01 16:37:31 +0700

    Добавьте файлы проекта.

---
+++ UDPClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+using var udpClient = new UdpClient();
+udpClient.Connect("127.0.0.1", 5555);
+Console.WriteLine("UDP-клиент запущен...");
+
+for (int i = 1; ; i++)
+{
+    // отправляемые данные
+    string message = i.ToString();
+    // преобразуем в массив байтов
+    byte[] data = Encoding.UTF8.GetBytes(message);
+
+    // отправляем данные
+    int bytes = await udpClient.SendAsync(data);
+    Console.WriteLine($"Отправлено {bytes} байт");
+
+    // получаем данные
+    var result = await udpClient.ReceiveAsync();
+    // предположим, что отправлена строка, преобразуем байты в строку
+    var resMessage = Encoding.UTF8.GetString(result.Buffer);
+
+    Console.WriteLine($"Получено {result.Buffer.Length} байт");
+    Console.WriteLine($"Удаленный адрес: {result.RemoteEndPoint}");
+    Console.WriteLine(resMessage);
+
+    Task.Delay(i * 100).Wait();
+}
+
---
+++ UDPServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+var localIP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
+
+// привязываем сокет к локальной точке 127.0.0.1:5555
+// и начинаем прослушивание входящих сообщений
+udpSocket.Bind(localIP);
+
+Console.WriteLine("UDP-сервер запущен...");
+
+byte[] data = new byte[256];
+
+EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
+while (true)
+{
+
+    var result = await udpSocket.ReceiveFromAsync(data, remoteIp);
+    var message = Encoding.UTF8.GetString(data, 0, result.ReceivedBytes);
+
+    Console.WriteLine($"Получено {result.ReceivedBytes} байт");
+    Console.WriteLine($"Удаленный адрес: {result.RemoteEndPoint}");
+    Console.WriteLine(message);
+
+    byte[] backData = Encoding.UTF8.GetBytes(message);
+
+    await udpSocket.SendToAsync(backData, result.RemoteEndPoint);
+}
